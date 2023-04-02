7cab96d4412ae9c8993b04502ec4a7015e096b91
Illia Kostenko <kosten.il2000@gmail.com>
2023-04-02 10:59:30 +0200

    feat: Initial db commits

---
+++ app/src/main/java/com/pv239/brnotouristapp/dao/PlaceDao.kt
@@ -0,0 +1,20 @@
+package com.pv239.brnotouristapp.dao
+
+import androidx.room.*
+import com.pv239.brnotouristapp.data.PlaceEntity
+
+@Dao
+interface PlaceDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun persist(entity: PlaceEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun persist(entity: List<PlaceEntity>)
+
+    @Query("SELECT * FROM PlaceEntity ORDER BY id ASC")
+    fun selectAllOrderById(): List<PlaceEntity>
+
+    @Delete
+    fun delete(entity: PlaceEntity)
+}
---
+++ app/src/main/java/com/pv239/brnotouristapp/data/PlaceEntity.kt
@@ -0,0 +1,21 @@
+package com.pv239.brnotouristapp.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import kotlinx.parcelize.Parcelize
+import java.net.URL
+import java.util.*
+
+
+@Entity
+data class PlaceEntity(
+    val id: Long,
+    val name: String,
+    val text: String,
+    val image: URL,
+    val urlLink: URL,
+
+    val contact: ContactEntity,
+    val address: AddressEntity
+
+)
