ab8fbdc53c909dbb0716fb64293469b60b9c43a7
“phoenixday” <xtsykyn@fi.muni.cz>
2023-04-24 14:01:45 +0200

    detail page (a little bit better)

--- app/src/main/java/com/pv239/brnotouristapp/place/PlaceDetails.kt
+++ app/src/main/java/com/pv239/brnotouristapp/place/PlaceDetails.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import coil.load
-import com.pv239.brnotouristapp.R
 import com.pv239.brnotouristapp.databinding.FragmentPlaceDetailsBinding
 
 class PlaceDetails : Fragment() {
